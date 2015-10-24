c6f6aa42d3cf37e6dd4545488246f922305e9a64
Anders Fischer-Nielsen <andersfischern@me.com>
2015-10-24 20:12:10 +0200

    Finished exercise2.

--- Assignment6/Exercise2/trans.fs
+++ Assignment6/Exercise2/trans.fs
@@ -1,5 +1,4 @@
-module trans
-
+module Trans
     type state = int
     
     type sym = 
@@ -13,30 +12,53 @@ module trans
             trans  : (state * sym * state) list
         }
         
+        
     let order nfa = 
         let extract (a, b, _) = a, b
         let sorted = Seq.sortBy (fun s -> extract s) nfa.trans 
                      |> List.ofSeq
                     
         { start = nfa.start; accept = nfa.accept; trans = sorted }
+        
+    let test1 = 
+        let s1 = { start = 0; 
+                   accept = 1; 
+                   trans = [ 
+                             (0, SChar 'b', 1);
+                             (0, SChar 'a', 2);
+                             (1, SChar 'p', 4);
+                             (4, SChar 'x', 1);
+                             (4, SChar 'y', 5);
+                             (2, SChar 'e', 3);
+                             (2, SChar 'i', 1);
+                             (3, SChar 'i', 2);
+                             (3, SChar 'a', 4) 
+                           ]
+                 }
+        order s1
+    
     
     let isDfa nfa = 
-        let sym = nfa.sym
-        if sym = SEps then false
+        let trans = nfa.trans
+        let noEps = List.forall (fun (_, b, _) -> b <> SEps) trans
+        let rec sameSym list =
+            let allDifferent f s l = 
+                List.forall (fun (a2, b2, _) -> f <> a2 && s <> b2) l
+            
+            match list with 
+            | (a, b, _)::xs -> if allDifferent a b xs
+                               then sameSym xs
+                               else false
+            | []            -> true
+            
+        noEps && (sameSym trans)
         
-        List.filter (fun s -> )
-        
-    
-    let test = 
-        let before = { start = 0; 
-                       accept = 1; 
-                       trans = [ (1, SChar 'b', 2); 
-                                 (1, SChar 'a', 2); 
-                                 (0, SChar 'c', 0); 
-                                 (0, SChar 'a', 3) ] 
-                     }
-        order before
-
-
-// Samples
-let s1 = {start = 0; accept = 1; trans = [(0,SChar 'b',1);(0,SChar 'a',2);(1,SChar 'p',4);(4,SChar 'x',1);(4,SChar 'y',5);(2,SChar 'e',3);(2,SChar 'i',1);(3,SChar 'i',2);(3,SChar 'a',4)]}
+    let test2 = 
+        let dfa = { start = 0; 
+                    accept = 1; 
+                    trans = [ 
+                              (0, SChar 'a', 1); 
+                              (0, SChar 'a', 2)
+                            ] 
+                  }
+        isDfa dfa
