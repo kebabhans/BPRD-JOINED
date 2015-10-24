6c0c182563be8f68aa284555a00b0e6245f10ba0
Morten Albertsen <moalb@itu.dk>
2015-10-24 19:17:58 +0200

    Super cool sample

--- Assignment6/Exercise2/trans.fs
+++ Assignment6/Exercise2/trans.fs
@@ -4,4 +4,6 @@ type nfa =
   { start  : state;
     accept : state;
     trans  : (state * sym * state) list
-  }
+  }// Samples
+let s1 = {start = 0; accept = 1; trans = [(0,SChar 'b',1);(0,SChar 'a',2);(1,SChar 'p',4);(4,SChar 'x',1);(4,SChar 'y',5);(2,SChar 'e',3);(2,SChar 'i',1);(3,SChar 'i',2);(3,SChar 'a',4)]}
+
