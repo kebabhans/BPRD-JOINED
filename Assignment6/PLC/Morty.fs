745868552d983d13a5ee0e5b634e11fc57ca257a
Morten Albertsen <moalb@itu.dk>
2015-10-28 14:22:43 +0100

    Morten's take on check function (thanks Wind)

--- Assignment6/PLC/Morty.fs
+++ Assignment6/PLC/Morty.fs
@@ -1,57 +1,53 @@
-let rec exists env x = match env with 
+// Author: Morten
+
+type expr = 
+  | CstI of int
+  | CstB of bool
+  | Var of string
+  | Let of string * expr * expr
+  | Prim of string * expr * expr
+  | If of expr * expr * expr
+  | Letfun of string * string * expr * expr    (* (f, x, fBody, letBody) *)
+  | Call of expr * expr
+
+let rec lookup env x = match env with
     | []        -> false
-    | (y, v)::r -> if x=y then true else exists r x
+    | y::xs     -> if y = x then true else lookup xs x
+
+
+let rec evaluate expr (environment : string list) = match expr with
+    | CstI i                      ->  true
+    | CstB b                      ->  true
+    | Let(name,e1,e2)             ->  let extEnvironment = name :: environment
+                                      (evaluate e1 extEnvironment && evaluate e2 extEnvironment)              // Might return true or false
+    | Prim(_,e1,e2)               ->  (evaluate e1 environment && evaluate e2 environment)
+    | Var s                       ->  lookup environment s
+    | If(e1,e2,e3)                ->  (evaluate e1 environment && evaluate e2 environment && evaluate e3 environment)
+    | Letfun(f,name, e1,e2)       ->  let extEnvironment = f :: name :: environment
+                                      (evaluate e1 extEnvironment && evaluate e2 extEnvironment)
+    | Call(e1,e2)                 ->  evaluate e1 environment && evaluate e2 environment
 
 
 
-let rec eval (e : expr) (env : value env) : value = match e with
-    | CstI i -> Int i
-    | CstB b -> Int (if b then 1 else 0)
-    | Var x  -> lookup env x
-    | Prim(ope, e1, e2) -> 
-      let v1 = eval e1 env
-      let v2 = eval e2 env
-      match (ope, v1, v2) with
-      | ("*", Int i1, Int i2) -> Int (i1 * i2)
-      | ("+", Int i1, Int i2) -> Int (i1 + i2)
-      | ("-", Int i1, Int i2) -> Int (i1 - i2)
-      | ("=", Int i1, Int i2) -> Int (if i1 = i2 then 1 else 0)
-      | ("<", Int i1, Int i2) -> Int (if i1 < i2 then 1 else 0)
-      |  _ -> failwith "unknown primitive or wrong type"
-    | Let(x, eRhs, letBody) -> 
-      let xVal = eval eRhs env
-      let letEnv = (x, xVal) :: env 
-      eval letBody letEnv
-    | If(e1, e2, e3) -> 
-      match eval e1 env with
-      | Int 0 -> eval e3 env
-      | Int _ -> eval e2 env
-      | _     -> failwith "eval If"
-    | Letfun(f, x, fBody, letBody) -> 
-      let bodyEnv = (f, Closure(f, x, fBody, env)) :: env
-      eval letBody bodyEnv
-    | Fun(x, funBody) -> 
-      Clos(x, funBody, env)
-    | Call(eFun, eArg) -> 
-      let fClosure = eval eFun env
-      match fClosure with
-      | Closure (f, x, fBody, fDeclEnv) ->
-        let xVal = eval eArg env
-        let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
-        in eval fBody fBodyEnv
-      | Clos (x, funBody, closEnv) ->
-        let xVal = eval eArg env
-        let newEnv = (x, xVal) :: closEnv
-        eval funBody closEnv
-      | _ -> failwith "eval Call: not a function";;
+let rec check expr = 
+    let environment : string list = []
+    evaluate expr environment
 
+// Samples
+let s2 = CstI(12)
+let s4 = CstB(false)
+let s5 = Let("x",Prim("+",Var "x", Var "x"),Call(Var "x",CstI(2)))
 
-let rec check expr = match expr with
-  | CstI i										  -> 	true
-  | CstB b										  -> 	true
-  | Var s										    -> 	exists s
-  | Let(s,e1,e2)								-> 	true // check e1 && check e2 + extend environment
-  | Prim(op,e1,e2)							-> 	true // Check e1 && e2 exists
-  | If(cond,e2,e3)							-> 	true // Check e1 && e2 exists 
-  | Letfun(name,e1,e2)					->  true // extend environment
-  | Call(e1,e2)									-> 	let env : string list = []
+let s12 = Letfun ("twice", "f",
+                  Letfun ("g", "x", Call (Var "f", Call (Var "f", Var "x")), Var "g"),
+                          Letfun ("mul3", "z", Prim ("*", Var "z", CstI 3),
+                                  Call (Call (Var "twice",Var "mul3"),CstI 2)))
+
+let s13 = Let("x",Prim("+",Var "y", Var "x"),Call(Var "x",CstI(2)))
+check s13     // Should produce false
+
+let s14 = Call(Var "f",CstI 2)
+check s14   // Should produce false
+
+check s1    // Should produce true
+check s2
